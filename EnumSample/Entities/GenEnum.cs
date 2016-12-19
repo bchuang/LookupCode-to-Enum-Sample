4a0147dd9a442104088075ed2e2a5235feb77e30
bchuang <boris855161@gmail.com>
2016-12-19 18:57:29 +0800

    Init

---
+++ EnumSample/Entities/GenEnum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EnumSample.Entities
+{
+
+    public enum AccountType : byte
+    {
+        SiteRegistered = 0,
+        Facebook = 1,
+        Google = 2,
+        Twitter = 3,
+    }
+
+
+    public enum EpaperStatus : byte
+    {
+        訂閱 = 1,
+        取消訂閱 = 2,
+    }
+
+
+    public enum BlacklistStatus : byte
+    {
+        否 = 0,
+        是 = 1,
+    }
+
+
+    public enum AccountStatus : byte
+    {
+        未啟用 = 0,
+        正常 = 1,
+        黑名單 = 2,
+        停用 = 3,
+    }
+
+}
---
+++ EnumSample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EnumSample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }   
+    }
+}
